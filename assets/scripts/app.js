281028402b6d8706809ed10c261716ab2077c323
Ulysseee <62332779+Ulysseee@users.noreply.github.com>
2021-05-14 16:48:40 +0200

    Fullscreen adjustments

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,7 +9,7 @@ let sudOuest = L.latLng(48.815003, 2.227135),
     nordEst = L.latLng(48.902724, 2.488421),
     bounds = L.latLngBounds(sudOuest, nordEst);
 
-let appUserInterface = document.querySelector('body');
+let appUserInterface = document.querySelector('html');
 let fullScreenBtn = document.querySelector('.fullScreen-btn');
 fullScreenBtn.addEventListener('click', event => {
     openFullscreen();
